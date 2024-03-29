f7d10f0b60c56d9ba47c7c96a1cb37203475bc8e
Runxi Yu <a@andrewyu.org>
2024-03-29 08:41:37 +0800

    Make test_template partially use current data

--- test_template.py
+++ test_template.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 from typing import Union, TypeAlias
 from jinja2 import Template, StrictUndefined
+import json
 from flask import (
     Flask,
     Response,
@@ -14,43 +15,34 @@ from flask import (
     make_response,
 )
 from werkzeug.wrappers.response import Response as werkzeugResponse
+from datetime import datetime
+import os
 
 ResponseType: TypeAlias = Union[Response, werkzeugResponse, str]
 
 app = Flask(__name__)
 app.jinja_env.undefined = StrictUndefined
 
-data = {
-    "aod": "FIXME",
+with open(
+    os.path.join("build", datetime.today().strftime("%Y%m%d") + "-data.json"), "r"
+) as fd:
+    data = json.load(fd)
+
+extra_data = {
+    "aod": data["aods"][0], # FIXME
     "stddate": "2024-04-01",
     "weekday_english": "Monday",
     "weekday_abbrev": "Mon",
     "next_weekday_abbrev": "Tue",
     "weekday_chinese": "周一",
     "day_of_cycle": "SA",
-    "community_time_days": [
-        [
-            "Mon",
-            "Whole School Assembly",
-            "Whole School Assembly",
-            "Whole School Assembly",
-            "Whole School Assembly",
-        ],
-        ["Tue", "Tutor Time", "Tutor Time", "Tutor Time", "Tutor Time"],
-        [
-            "Wed",
-            "Academic Assembly: PPE Recongition (Gym)",
-            "Dean's Forum (Auditorium)",
-            "Zhi Xing PRE-trip (various location)",
-            "Health Talk 4\xa0 (Auditorium)",
-        ],
-        ["Thu", "Tutor Time", "Tutor Time", "Tutor Time", "Tutor Time"],
-    ],
     "today_breakfast": ("1", "2", "3", "4", "5", "6", "7", "8"),
     "today_lunch": ("1", "2", "3", "4", "5", "6", "7", "8"),
     "today_dinner": ("1", "2", "3", "4", "5", "6", "7", "8"),
     "next_breakfast": ("1", "2", "3", "4", "5", "6", "7", "8"),
 }
+
+data = data | extra_data
 
 
 @app.route("/")
