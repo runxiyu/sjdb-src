d130d42cd86f51b5505bfdb11841a9c499f7609d
Runxi Yu <a@andrewyu.org>
2024-03-29 09:23:28 +0800

    Note to escape the HTML in jinja2

--- pack.py
+++ pack.py
@@ -3,3 +3,5 @@ from jinja2 import Template, StrictUndefined
 with open("templates/template.html", "r") as template_file:
     template = Template(template_file.read(), undefined=StrictUndefined)
 template.stream(**data).dump("<++>.html")
+
+# FIXME: Escape the dangerous HTML!
