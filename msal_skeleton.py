c59ca294bc28479f4b1e3bc4e37ea7fd09db8300
Runxi Yu <a@andrewyu.org>
2024-04-01 12:39:44 +0800

    msal_skeleton.py: Base script to base MS Graph operations on

---
+++ msal_skeleton.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+import logging
+import msal  # type: ignore
+import requests
+import datetime
+from configparser import ConfigParser
+from typing import Any, Optional
+import json
+
+
+def acquire_token(app: msal.PublicClientApplication, config: ConfigParser) -> str:
+    result = app.acquire_token_by_username_password(
+        config["credentials"]["username"],
+        config["credentials"]["password"],
+        scopes=config["credentials"]["scope"].split(" "),
+    )
+
+    if "access_token" in result:
+        assert type(result["access_token"]) is str
+        return result["access_token"]
+    else:
+        raise ValueError("Authentication error in password login")
+
+# TODO
+def something(token: str) -> dict[str, Any]:
+    return requests.get("https://graph.microsoft.com/v1.0/me", headers={"Authorization": "Bearer " + token}).json()
+
+def main() -> None:
+    config = ConfigParser()
+    config.read("config.ini")
+    app = msal.PublicClientApplication(
+        config["credentials"]["client_id"],
+        authority=config["credentials"]["authority"],
+    )
+    token = acquire_token(app, config)
+    print(something(token))
+    # TODO
+
+
+if __name__ == "__main__":
+    main()
