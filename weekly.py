96b73f142fc24fb8c95e2894281e1292dc993786
Runxi Yu <me@runxiyu.org>
2024-05-18 11:01:55 +0800

    *.py: Fix license statements

--- weekly.py
+++ weekly.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 #
-# Weekly script to prepare the YK Pao School Daily Bulletin
-# Copyright (C) 2024  Runxi Yu <https://runxiyu.org>
+# Weekly script to prepare the YK Pao School Daily Bulletin Copyright (C) 2024
+# Runxi Yu <https://runxiyu.org>
 #
-# This program is free softhe_week_aheadre: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Softhe_week_aheadre Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free softhe_week_aheadre: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Softhe_week_aheadre Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
