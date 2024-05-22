1967a2ae9879eac4d0cb7b9d4e68e55c095125e7
Runxi Yu <me@runxiyu.org>
2024-05-22 11:26:30 +0800

    Fix broken python file licensing

--- daily.py
+++ daily.py
@@ -3,15 +3,15 @@
 # Daily script to prepare the YK Pao School Daily Bulletin's JSON data files
 # Copyright (C) 2024 Runxi Yu <https://runxiyu.org>
 #
-# This program is free softhe_week_aheadre: you can redistribute it and/or
-# modify it under the terms of the GNU Affero General Public License as
-# published by the Free Softhe_week_aheadre Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
-# details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
