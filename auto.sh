59e62dac7c03bbd2b49e13bd8adf0f83951b243d
Runxi Yu <me@runxiyu.org>
2024-05-25 13:50:15 +0800

    auto.sh: Exit on fail

--- auto.sh
+++ auto.sh
@@ -11,9 +11,9 @@ fi
 
 if [ "$(date -d "$TARGET" +"%a")" = "Mon" ]
 then
-	python3 weekly.py --date="$TARGET"
+	python3 weekly.py --date="$TARGET" || exit 1
 fi
 
-python3 daily.py --date="$TARGET"
-python3 pack.py --date="$TARGET"
-python3 sendmail.py --date="$TARGET"
+python3 daily.py --date="$TARGET" || exit 2
+python3 pack.py --date="$TARGET" || exit 3
+python3 sendmail.py --date="$TARGET" || exit 4
