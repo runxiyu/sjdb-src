c5d345a31c32bb893ab0e8d4b121e3ad4185f7f6
Runxi Yu <me@runxiyu.org>
2024-05-25 13:58:14 +0800

    auto.sh: Don't generate for weekends

--- auto.sh
+++ auto.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-set -x
-
 if [ -z "$1" ]
 then
 	TARGET="$(date +"%Y-%m-%d")"
 else
 	TARGET="$1"
 fi
+printf 'Target: %s\n' "$TARGET"
+
+if [ "$(date -d "$TARGET" +"%a")" = "Sun" ] || [ "$(date -d "$TARGET" +"%a")" = "Sat" ]
+then
+	printf 'Not generating for weekends, exiting\n' >&2
+	exit 5
+fi
 
 if [ "$(date -d "$TARGET" +"%a")" = "Mon" ]
 then
+	printf 'Target day is a Monday, running weekly.py too\n' >&2
+	exit 5
 	python3 weekly.py --date="$TARGET" || exit 1
 fi
 
+printf 'Generating...'
 python3 daily.py --date="$TARGET" || exit 2
 python3 pack.py --date="$TARGET" || exit 3
 python3 sendmail.py --date="$TARGET" || exit 4
