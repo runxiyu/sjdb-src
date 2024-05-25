5800d0f0a263a58163f8faa1df861e46bc6f0c35
Runxi Yu <me@runxiyu.org>
2024-05-25 09:36:27 +0800

    inspire_approve.py: Simple script to approve inspirations

---
+++ inspire_approve.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+import json
+import sys
+
+for fn in sys.argv[1:]:
+    with open(fn, "r") as fd:
+        jq = json.load(fd)
+        jq["approved"] = True
+        json.dump(fd, jq, indent="\t")
