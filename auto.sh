e9ff56273743745ab2ee7676048799f71c0b0fd5
Runxi Yu <me@runxiyu.org>
2024-06-19 08:00:00 +0800

    auto.sh: Let the operator check the bulletin before sending

--- auto.sh
+++ auto.sh
@@ -24,5 +24,12 @@ printf 'Running daily.py\n' >&2
 python3 daily.py --date="$TARGET" || exit 2
 printf 'Running pack.py\n' >&2
 python3 pack.py --date="$TARGET" || exit 3
-printf 'Running sendmail.py\n' >&2
-python3 sendmail.py --date="$TARGET" || exit 4
+xdg-open ../sjdb-build/sjdb-"$(printf '%s' "$TARGET" | tr -d '-')".html
+read -p "Is this bulletin OK to send? [y/N] " res
+if [ "$res" = "y" ]
+then
+	printf 'Running sendmail.py\n' >&2
+	python3 sendmail.py --date="$TARGET" || exit 4
+else
+	printf 'Not sending this bulletin. You may run sendmail manually instead:\npython3 sendmail.py --date=%s\n' "$TARGET" >&2
+fi
